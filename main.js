e8de05d130f33e081f73dab19ae5129274895302
Onwugbenu Chisom <onwugbenuglory@gmail.com>
2020-07-09 18:50:32 +0100

    my first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+/*const quiz = [
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonder Woman's real name?","Diana Prince"],
+    ["What is Batman's real name?","Bruce Wayne"],
+    ["What is Captain America's real name?", "Steve Rogers"],
+    ["What is Spiderman's real mame?", "Peter Parker"]
+    ];
+    
+    let score = 0;
+    
+    for(const [question,answer] of quiz){
+    const response = prompt(question);
+    if(response === answer){
+    alert('Correct!');
+    score++;
+    } else {
+    alert(`Wrong! The correct answer was ${answer}`);
+    }
+    }
+    alert(`Game Over, you scored ${score} point${score !== 1 ?
+    's' : ''}`);*/
